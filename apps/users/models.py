3fb846ca830653ef083d7a7f5cc014d5647d5c20
Veronika <veronika.kustermann@scandio.de>
2020-12-21 21:28:39 +0100

    Users visible for admin and registering as trainer

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Roles(models.TextChoices):
+    ADMIN = 'admin', _('Admin')
+    TRAINER = 'trainer', _('Trainer')
+    USER = 'user', _('User')
 
 
 class User(AbstractUser):
-    role_choices = [
-        ('trainer', 'Trainer'),
-        ('user', 'User'),
-        ('admin', 'Admin'),
-    ]
-
-    role = CharField(
+    role = models.CharField(
         max_length=150,
-        choices=role_choices,
-        default='user',
+        choices=Roles.choices,
+        default=Roles.USER,
     )
