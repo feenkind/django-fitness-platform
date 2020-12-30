5d5f10dfbd157393bf444b4c3ccf54915385c7a0
Diana Schilling <diana.maria.schilling@gmail.com>
2020-12-30 09:17:14 -0800

    Add Location Table

--- apps/trainers/admin.py
+++ apps/trainers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trainer, Upload
+from .models import Trainer, Upload, Location
 
 admin.site.register(Trainer)
 admin.site.register(Upload)
+admin.site.register(Location)
--- apps/trainers/models.py
+++ apps/trainers/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from fitnessplatform import settings
 
+
+# class Sports(models.TextChoices):
+#     YOGA = 'yoga',
+
 class Trainer(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     sport = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
     motto = models.CharField(max_length=100)
-    about = models.CharField(max_length=200)
+    about = models.CharField(max_length=500)
 
     def __str__(self):
         return self.user.username
@@ -18,3 +21,13 @@ class Upload(models.Model):
     def __str__(self):
         return f'{self.trainer.__str__()} ({self.url})'
 
+class Location(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    street = models.CharField(max_length=40)
+    number = models.CharField(max_length=5)
+    zipcode = models.CharField(max_length=10)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.trainer.__str__()
