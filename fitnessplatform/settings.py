72a656a6cdb60204ae9d705f058cf820b0e29c3d
Veronika <veronika.kustermann@scandio.de>
2021-01-01 21:37:13 +0100

    Show dummy avatar

--- fitnessplatform/settings.py
+++ fitnessplatform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 
 env = environ.Env()
@@ -144,6 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
