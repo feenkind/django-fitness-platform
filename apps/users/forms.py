8a16be7ca3068458fc715bb1e05b4f0a27ce8d50
Veronika <veronika.kustermann@scandio.de>
2021-01-02 18:46:22 +0100

    Basic profile settings edit form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,11 +1,15 @@
 from allauth.account.forms import SignupForm
 from django import forms
-from .models import Roles
+from .models import Roles, User
 
 
 class UsersSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=30, label='First Name', initial='First Name')
-    last_name = forms.CharField(max_length=30, label='Last Name', initial='Last Name')
+    first_name = forms.CharField(
+        max_length=30, label='First Name', initial='First Name'
+    )
+    last_name = forms.CharField(
+        max_length=30, label='Last Name', initial='Last Name'
+    )
 
     def save(self, request):
         # Ensure you call the parent class's save.
@@ -16,3 +20,9 @@ class UsersSignupForm(SignupForm):
         user.save()
 
         return user
+
+
+class UserSettings(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
