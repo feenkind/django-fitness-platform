0e8f2215fd332b5f2ec2075cf510784f447b8404
Veronika <veronika.kustermann@scandio.de>
2021-01-02 19:10:00 +0100

    Required fields when editing profile

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -23,6 +23,10 @@ class UsersSignupForm(SignupForm):
 
 
 class UserSettings(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
