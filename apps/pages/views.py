dd135a98bcc2d47a843d7c57f0dc89c71225864b
vinc-rot <vrhack1@gmail.com>
2021-01-02 12:22:36 +0100

    Merge remote-tracking branch 'origin/Design' into Design
    # Conflicts:
    #	fitnessplatform/settings.py

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -7,15 +7,3 @@ def start_view(request, *args, **kwargs):
     }
     return render (request, "pages/startpage.html", context)
 
-
-def fitnessplans_view(request, *args, **kwargs):
-    context = {
-        "page_title": "Fitnesspläne"
-    }
-    return render(request, 'pages/fitnessplans.html', context)
-
-def food_view(request, *args, **kwargs):
-    context = {
-        "page_title": "Gesunde Ernährung"
-    }
-    return render(request, 'pages/healthyfood.html', context)
