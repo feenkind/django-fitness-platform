d8fc923835a8bb59ec9a8a1999b08a4f599f3e2a
vinc-rot <vrhack1@gmail.com>
2021-01-03 11:53:33 +0100

    Merge remote-tracking branch 'origin/Design' into Design
    # Conflicts:
    #	fitnessplatform/settings.py

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
 
 def start_view(request, *args, **kwargs):
     context = {
-        "page_title": "Startseite"
+        'page_title': 'Startseite'
     }
-    return render (request, "pages/startpage.html", context)
+    return render (request, 'pages/startpage.html', context)
 
