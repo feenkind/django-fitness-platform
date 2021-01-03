774555354df0924c9e7afd96fa56532116b6ffcd
Veronika <veronika.kustermann@scandio.de>
2021-01-03 11:34:12 +0100

    Formatting

--- fitnessplatform/settings.py
+++ fitnessplatform/settings.py
@@ -153,7 +153,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = 'users.User'
 
 ACCOUNT_FORMS = {
     'signup': 'apps.users.forms.UsersSignupForm',
