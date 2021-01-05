daff71a46085a3e637c77f8be188a2fc007ebe59
Veronika <veronika.kustermann@scandio.de>
2021-01-05 16:56:30 +0100

    Overwrite existing avatars

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+import os
 
 
 class Roles(models.TextChoices):
@@ -17,10 +19,23 @@ class User(AbstractUser):
     )
 
     def user_directory_path(self, filename):
-        # file will be uploaded to MEDIA_ROOT/user_<id>/avatar/<filename>
-        return f'user_{self.id}/avatar/avatar.{filename.split(".")[1]}'
+        # file will be uploaded to MEDIA_ROOT/user_<id>/avatar/avatar.<extension>
+        name = f'avatar.{filename.split(".")[1]}'
+        path = f'user_{self.id}/avatar/'
+        fullpath = os.path.join(settings.MEDIA_ROOT, path)
+        if os.path.exists(fullpath):
+            for filename in os.listdir(fullpath):
+                # remove any exiting avatar file
+                if filename.startswith('avatar'):
+                    os.remove(f'{fullpath}/{filename}')
 
-    avatar = models.ImageField(upload_to=user_directory_path, null=True)
+        return f'{path}/{name}'
+
+    avatar = models.ImageField(
+        upload_to=user_directory_path,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f'{self.username} - {self.email}'
