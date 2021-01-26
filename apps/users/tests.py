66443a25a1e19fdcc91d6881fe6b3fe3725b62bb
Veronika <feenkind@gmail.com>
2021-01-26 21:23:22 +0100

    Test for profile settings

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from apps.users.models import User
 
-# Create your tests here.
+
+class UserSettingsTestCase(TestCase):
+    def test_not_logged_in(self):
+        response = self.client.get(reverse('user_profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please login')
+
+    def test_logged_in(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='12345'
+        )
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('user_profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Your profile settings')
